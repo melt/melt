e628e2b5e69db6675dbffd6484cd99e404c9d144
Hannes Landeholm <hannes@recurringly.com>
2010-02-19 12:34:17 +0000

    Removed "cake compatibility" file. Unneeded.

--- sys/system.php
+++ sys/system.php
@@ -1,7 +1,6 @@
 <?php
 /**
 * @desc nanoMVC System File - The Functional and UltraLight CMS.
-* @version 0.1
 */
 
 // Initialize the request.
@@ -15,9 +14,6 @@ require "sys.Culture.php";
 
 // Error catching.
 require "sys.Catcher.php";
-
-// Cake compability.
-require "sys.Cake.php";
 
 // Handle the request.
 require "sys.Request.php";
