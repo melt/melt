88c4808917905ca641c80c329c9cf0267ed2e465
Hannes Landeholm <hannes@recurringly.com>
2010-06-12 06:15:06 +0000

    Bootstrap is not used. Redirecting directly too core now.

--- bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// If you installed your core on another location you should not change this
-// but replace the "core" directory with a symlink.
-// Changing this also requires you to change the
-// core static path in the .htacccess.
-require "core/core.php";
