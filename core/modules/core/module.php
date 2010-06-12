b113b92edd936828215df28e6c2676fe2c6505f2
Hannes Landeholm <hannes@recurringly.com>
2010-06-12 06:40:12 +0000

    Core module now provides a helpful debug() function for JS.

--- core/modules/core/module.php
+++ core/modules/core/module.php
@@ -1,5 +1,10 @@
-<?php
+<?php namespace nmvc\core;
 
-namespace nmvc\core;
+class CoreModule extends \nmvc\CoreModule {
 
-class CoreModule extends \nmvc\CoreModule { }
+    public static function beforeRender() {
+        if (APP_IN_DEVELOPER_MODE)
+            \nmvc\View::render("/core/include_pretty_print", null, false, true);
+    }
+
+}
---
+++ core/modules/core/views/include_pretty_print.php
@@ -0,0 +1,4 @@
+<?php namespace nmvc; ?>
+<?php $this->layout->enterSection("head"); ?>
+<script type="text/javascript" src="<?php echo url("/static/cmod/core/prettyprint.js"); ?>"></script>
+<?php $this->layout->exitSection(); ?>
