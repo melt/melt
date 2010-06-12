658be14f979ee1220f6b9420e49e71f7fbf2243c
Hannes Landeholm <hannes@recurringly.com>
2010-06-12 06:36:23 +0000

    Request module improvements. send_json_data() now available. show_info now uses a controller so 4xx messages is now integrated in site design.

---
+++ core/modules/request/controllers/info_controller.php
@@ -0,0 +1,9 @@
+<?php namespace nmvc\request;
+
+class InfoController extends \nmvc\AppController {
+    function _show($topic, $body) {
+        $this->topic = $topic;
+        $this->body = $body;
+        return "/request/info/show";
+    }
+}
