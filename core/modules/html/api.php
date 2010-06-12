c1838aeaacbb709dc0b6b0db9da1e64177cc652a
Hannes Landeholm <hannes@recurringly.com>
2010-06-12 06:47:49 +0000

    escape() now handles broken UTF-8 strings more nicely than just returning null.

--- core/modules/html/api.php
+++ core/modules/html/api.php
@@ -13,7 +13,18 @@ function write($head, $body) {
 
 /** Escapes given string so it can be safely printed in HTML. */
 function escape($string) {
-    return htmlspecialchars($string, ENT_COMPAT, 'UTF-8');
+    $ret = htmlspecialchars($string, ENT_COMPAT, 'UTF-8');
+    if ($string != "" && $ret == "") {
+        // Invalid UTF-8. Gusss that string is iso-8859-1.
+        // This will never crash as iso-8859-1 does not have invalid characher
+        // sequences. (All octet's are defined.) However, it will produce
+        // invalid charachers if the input encoding is not ISO-8859-1.
+        // However, that is a rule violation anyway as all strings in nanoMVC
+        // SHOULD be encoded in UTF-8.
+        $string = iconv("ISO-8859-1", "UTF-8", $string);
+        return htmlspecialchars($string, ENT_COMPAT, 'UTF-8');
+    }
+    return $ret;
 }
 
 /** Decodes given HTML string to UTF-8. */
