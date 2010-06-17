97ea24861b3dc5500012ec921ebeb23d5c11305d
Hannes Landeholm <hannes@recurringly.com>
2010-06-17 20:09:55 +0000

    Cleanup.

--- core/modules/request/classes/class.Flash.php
+++ core/modules/request/classes/class.Flash.php
@@ -1,14 +1,10 @@
-<?php
-
-namespace \nmvc\request;
+<?php namespace \nmvc\request;
 
 class Flash {
     public static $flash_message = null;
     public static $flash_status = null;
 
-    /**
-    * @desc Returns an array of flasher variable references that can be used in template.
-    */
+    /** Returns an array of flasher variable references that can be used in template. */
     public static function getFlashRef() {
         if (isset($_SESSION['next_flash']) && is_array($_SESSION['next_flash'])) {
             list(self::$flash_message, self::$flash_status) = $_SESSION['next_flash'];
@@ -20,17 +16,13 @@ class Flash {
         );
     }
 
-    /**
-    * @desc Flashes a message inline in the current request.
-    */
+    /** Flashes a message inline in the current request. */
     public static function doFlash($message, $status = FLASH_BAD) {
         self::$flash_message = $message;
         self::$flash_status = $status;
     }
 
-    /**
-    * @desc Redirects and then flashes a message.
-    */
+    /** Redirects and then flashes a message. */
     public static function doFlashRedirect($url, $message, $status = FLASH_BAD) {
         $_SESSION['next_flash'] = array($message, $status);
         redirect($url);
--- core/modules/request/controllers/info_controller.php
+++ core/modules/request/controllers/info_controller.php
@@ -4,6 +4,6 @@ class InfoController extends \nmvc\AppController {
     function _show($topic, $body) {
         $this->topic = $topic;
         $this->body = $body;
-        return "/request/info/show";
+        return "/request/info";
     }
 }
