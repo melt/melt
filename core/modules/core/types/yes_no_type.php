10e7e4363d1de3191ff52d5af467d49e9a2deaf6
Hannes Landeholm <hannes@recurringly.com>
2010-06-21 16:32:56 +0000

    Fixed bug when setting true_str, false_str.

--- core/modules/core/types/yes_no_type.php
+++ core/modules/core/types/yes_no_type.php
@@ -9,6 +9,14 @@ class YesNoType extends \nmvc\AppType {
     public $true_str = null;
     public $false_str = null;
 
+    private function yes() {
+        return $this->true_str == null? STR_YES: escape($this->true_str);
+    }
+
+    private function no() {
+        return $this->false_str == null? STR_NO: escape($this->false_str);
+    }
+
     public function getSQLType() {
         return "tinyint(1)";
     }
@@ -21,15 +29,15 @@ class YesNoType extends \nmvc\AppType {
             $no_check = "";
         else
             $yes_check = "";
-        $yes = $this->true_str == null? STR_YES: $this->true_str;
-        $no = $this->false_str == null? STR_NO: $this->false_str;
+        $yes = $this->yes();
+        $no = $this->no();
         return "<select name=\"$name\" id=\"$name\"><option value=\"0\" $no_check>$no</option><option value=\"1\" $yes_check>$yes</option></select>";
     }
     public function readInterface($name) {
         $this->value = (@$_POST[$name] == true);
     }
     public function __toString() {
-        return $this->value? STR_YES: STR_NO;
+        return $this->value? $this->yes(): $this->no();
     }
 }
 
