7dc417da84138b2a0dbb274c7bce6c47f1a84477
Hannes Landeholm <hannes@recurringly.com>
2010-06-26 19:21:08 +0000

    Added useful function array_merge_recursive_distinct.

--- core/modules/core/api.php
+++ core/modules/core/api.php
@@ -79,6 +79,37 @@ function compare_arrays($array1, $array2) {
 }
 
 /**
+ * Like array_merge_recursive but does not create new arrays
+ * by merging colliding values, instead replacing the value with
+ * the latter choise.
+ * @see http://se2.php.net/manual/en/function.array-merge-recursive.php
+ * @author mark dot roduner at gmail dot com
+ * @return array
+ */
+function array_merge_recursive_distinct() {
+    $arrays = func_get_args();
+    $base = array_shift($arrays);
+    if (!is_array($base))
+        $base = empty($base)? array(): array($base);
+    foreach ($arrays as $append) {
+        if (!is_array($append)) $append = array($append);
+        foreach ($append as $key => $value) {
+            if(!array_key_exists($key, $base) and !is_numeric($key)) {
+                $base[$key] = $append[$key];
+                continue;
+            }
+            if (is_array($value) or is_array($base[$key]))
+                $base[$key] = array_merge_recursive_distinct($base[$key], $append[$key]);
+            else if (is_numeric($key))
+                if(!in_array($value, $base)) $base[] = $value;
+            else
+                $base[$key] = $value;
+        }
+    }
+    return $base;
+}
+
+/**
  * Specifies that module is required to proceed with request.
  * It will check if it exists and satisfies given version.
  * If it doesn't, the function will terminate the request in either a 404 or
