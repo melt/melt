159264c48784f9173b7a75833a9de7a1fa2c7627
Hannes Landeholm <hannes@recurringly.com>
2010-08-02 16:13:51 +0000

    Improved prototype method signatures and phpdoc.

--- app_controller.php
+++ app_controller.php
@@ -6,22 +6,29 @@ abstract class AppController extends Controller {
     public $layout = "/html/xhtml1.1";
 
     /**
-     * This function is executed before every action in the controller.
+     * This function is executed before any action in the controller.
      * It's a handy place to check for an active session or
      * inspect user permissions.
-     * @param $action_name Action name about to be called. This function may
-     * be called with additional parameters.
+     * @param string $action_name Action name about to be called.
+     * @param array $arguments Arguments that will be passed to action.
+     * @return void
      */
-    public function beforeFilter($action_name) {}
+    public function beforeFilter($action_name, $arguments) {}
 
     /**
      * Called after controller action logic, but before the view is rendered.
+     * @param string $action_name Action name that was called.
+     * @param array $arguments Arguments that was passed to action.
+     * @return void
      */
-    public function beforeRender() {}
+    public function beforeRender($action_name, $arguments) {}
 
     /**
      * Called after every controller action, and after rendering is complete.
      * This is the last controller method to run.
+     * @param string $action_name Action name that was called.
+     * @param array $arguments Arguments that was passed to action.
+     * @return void
      */
-    public function afterRender() {}
+    public function afterRender($action_name, $arguments) {}
 }
