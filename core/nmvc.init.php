255fb2d669c7f27770482c4dacae5d8576ad9f37
Hannes Landeholm <hannes@recurringly.com>
2010-08-03 13:30:55 +0000

    About to push version 1.6.0. Refactored initialization. All initialization scripts now run in nmvc\internal. Added internal\put_configuration_directive(). nanoMVC now autoconfigures everything and it now trusts the webserver configuration and autoconfigures it's protocol, host, port and path from it. Instead added restriction directives. Init now parses REQ_URL instead of routing.

--- core/nmvc.init.php
+++ core/nmvc.init.php
@@ -1,61 +1,110 @@
-<?php
+<?php namespace nmvc\internal;
 
-namespace nmvc {
-    // Core constants.
-    const VERSION = "1.5.4";
-    define("APP_CORE_DIR", dirname(__FILE__));
-    // Standard function to restore the working dir to the core directory.
-    function restore_workdir() {
-        if (chdir(APP_CORE_DIR) !== true)
-            trigger_error("nanoMVC: Could not flip working directory to core.", \E_USER_ERROR);
-    }
-    restore_workdir();
-    // Send Powered By Header.
-    header("X-Powered-By: nmvc/" . VERSION, false);
-    // Explicitly set the default timezone if neccessary.
-    if (function_exists("date_default_timezone_set"))
-        date_default_timezone_set(@date_default_timezone_get());
-    // Using UTF-8 for everything.
-    iconv_set_encoding("internal_encoding", "UTF-8");
-    iconv_set_encoding("output_encoding", "UTF-8");
-    // Start session.
-    session_start();
-    // Walk down from the script filename to get the app dir.
-    // Note: This method is compatible with symbolic links.
-    $app_dir = dirname(dirname($_SERVER['SCRIPT_FILENAME']));
+// Core constants.
+const VERSION = "1.6.0";
+define("APP_CORE_DIR", dirname(__FILE__));
+// Standard function to restore the working dir to the core directory.
+function restore_workdir() {
+    if (chdir(APP_CORE_DIR) !== true)
+        trigger_error("nanoMVC: Could not flip working directory to core.", \E_USER_ERROR);
 }
-
-namespace nmvc\config {
-    /**
-     * Configures the non-core modules the application is using
-     * (once, in config.php) and then returns them.
-     * @return array Configured modules the application is using.
-     */
-    function modules_using() {
-        static $use_modules = null;
-        if ($use_modules === null)
-            $use_modules = func_get_args();
-        else
-            return $use_modules;
-    }
-    // Read configuration, local configuration first if it exist.
-    define("APP_DIR", $app_dir);
-    $local_config = APP_DIR . "/config.local.php";
-    define("APP_CONFIG", is_file($local_config)? $local_config: APP_DIR . "/config.php");
-    require APP_CONFIG;
-    if (modules_using() === null)
-        trigger_error("nanoMVC: config.php did not set what modules the application is using.", \E_USER_ERROR);
-    // Evaluate developer mode based on configuration and cookies.
-    $devkey_is_blank = DEV_KEY == "";
-    $devkey_matches = isset($_COOKIE['devkey']) && ($_COOKIE['devkey'] === DEV_KEY);
-    define("APP_IN_DEVELOPER_MODE", MAINTENANCE && ($devkey_is_blank || $devkey_matches));
-    $root_host = parse_url(ROOT_URL, PHP_URL_HOST);
-    $root_port = parse_url(ROOT_URL, PHP_URL_PORT);
-    $root_port = ($root_port != "" && $root_port != "80")? ":$root_port": "";
-    define("APP_ROOT_HOST", $root_host . $root_port);
-    define("APP_ROOT_PATH", parse_url(ROOT_URL, PHP_URL_PATH));
-    $port = parse_url(ROOT_URL, PHP_URL_PORT);
-    if (!$port || $port == "")
-        $port = 80;
-    define("APP_ROOT_PORT", $port);
+restore_workdir();
+// Send Powered By Header.
+header("X-Powered-By: nmvc/" . VERSION, false);
+// Explicitly set the default timezone if neccessary.
+if (function_exists("date_default_timezone_set"))
+    date_default_timezone_set(@date_default_timezone_get());
+// Using UTF-8 for everything.
+iconv_set_encoding("internal_encoding", "UTF-8");
+iconv_set_encoding("output_encoding", "UTF-8");
+// Start session.
+session_start();
+// Walk down from the script filename to get the app dir.
+// Note: This method is compatible with symbolic links.
+$app_dir = dirname(dirname($_SERVER['SCRIPT_FILENAME']));
+/**
+ * Configures the non-core modules the application is using
+ * (once, in config.php) and then returns them.
+ * @return array Configured modules the application is using.
+ */
+function modules_using() {
+    static $use_modules = null;
+    if ($use_modules === null)
+        $use_modules = func_get_args();
+    else
+        return $use_modules;
 }
+/**
+ * Puts a configuration directive in the configuration file.
+ * If configuration already exists, it is not added.
+ * @param string $config_var_fqn
+ * @param mixed $default_value
+ */
+function put_configuration_directive($config_var_fqn, $default_value) {
+    if (defined($config_var_fqn))
+        return;
+    define($config_var_fqn, $default_value);
+    // Add constant to application configuration.
+    $config_file_data = file_get_contents(APP_CONFIG);
+    $default_value = var_export($default_value, true);
+    $namespace = dirname($config_var_fqn);
+    $config_var_name = basename($config_var_fqn);
+    if ($config_var_name != strtoupper($config_var_name))
+        trigger_error("Configuration constants must be upper case, '$config_var_fqn' is not.", \E_USER_ERROR);
+    if (preg_match('#namespace\s+' . str_replace("\\",  "\\\\", $namespace) . '\s*{#si', $config_file_data, $match, \PREG_OFFSET_CAPTURE)) {
+        // Insert.
+        $offset = $match[0][1] + strlen($match[0][0]);
+        $config_file_data = substr($config_file_data, 0, $offset)
+        . "\r\n    const $config_var_name = $default_value;"
+        . substr($config_file_data, $offset);
+    } else {
+        // Append.
+        $config_file_data .= "\r\n\r\nnamespace $namespace {\r\n    const $config_var_name = $default_value;\r\n}\r\n";
+    }
+    file_put_contents(APP_CONFIG, $config_file_data);
+}
+/**
+ * Reads a $_SERVER variable.
+ */
+function read_server_var($var_name) {
+    if (!array_key_exists($var_name, $_SERVER))
+        trigger_error("nanoMVC initialization failed: Required \$_SERVER variable '$var_name' is not set! Webserver/PHP incompability?", \E_USER_ERROR);
+    return $_SERVER[$var_name];
+}
+// Read configuration, local configuration first if it exist.
+define("APP_DIR", $app_dir);
+$local_config = APP_DIR . "/config.local.php";
+define("APP_CONFIG", is_file($local_config)? $local_config: APP_DIR . "/config.php");
+require APP_CONFIG;
+if (modules_using() === null)
+    trigger_error("nanoMVC initialization failed: config.php did not set what modules the application is using.", \E_USER_ERROR);
+// Make sure core directives that it requires right away was included.
+put_configuration_directive('nmvc\core\config\DEVELOPER_KEY', "");
+put_configuration_directive('nmvc\core\config\MAINTENANCE_MODE', true);
+put_configuration_directive('nmvc\core\config\REQUIRE_PROTOCOL', null);
+put_configuration_directive('nmvc\core\config\REQUIRE_HOST', "#^localhost$#");
+put_configuration_directive('nmvc\core\config\REQUIRE_PATH', null);
+put_configuration_directive('nmvc\core\config\REQUIRE_PORT', "#^80$#");
+put_configuration_directive('nmvc\core\config\REDIRECT_URL', null);
+put_configuration_directive('nmvc\core\config\FORCE_ERROR_DISPLAY', false);
+put_configuration_directive('nmvc\core\config\FORCE_ERROR_FLAGS', false);
+// Evaluate developer mode based on configuration and cookies.
+$devkey_is_blank = \nmvc\core\config\DEVELOPER_KEY == "";
+$devkey_matches = isset($_COOKIE['NMVC_DEVKEY']) && ($_COOKIE['NMVC_DEVKEY'] === \nmvc\core\config\DEVELOPER_KEY);
+define("APP_IN_DEVELOPER_MODE", \nmvc\core\config\MAINTENANCE_MODE && ($devkey_is_blank || $devkey_matches));
+// Evaluate application host, path, port and protocol
+// which is determined by the server and optionally restricted by configuration.
+$php_self = read_server_var("PHP_SELF");
+if (!preg_match('#/core/core\.php$#', $php_self))
+    trigger_error("nanoMVC initialization failed: PHP_SELF does not end with '/core/core.php'. Make sure the core is installed properly.");
+define("APP_ROOT_PROTOCOL", (isset($_SERVER["HTTPS"]) && !empty($_SERVER["HTTPS"]))? "https": "http");
+define("APP_ROOT_HOST", preg_replace('#:[\d]+$#', "", read_server_var("HTTP_HOST")));
+define("APP_ROOT_PORT", $server_port = intval(read_server_var('SERVER_PORT')));
+define("APP_ROOT_PATH", substr($php_self, 0, -strlen("/core/core.php")));
+define("APP_USING_STANDARD_PORT", (APP_ROOT_PROTOCOL == "http" && APP_ROOT_PORT == 80) || (APP_ROOT_PROTOCOL == "https" && APP_ROOT_PORT == 443));
+define("APP_ROOT_URL", APP_ROOT_PROTOCOL . "://" . APP_ROOT_HOST . (APP_USING_STANDARD_PORT? "": ":" . APP_ROOT_PORT) . APP_ROOT_PATH);
+// Parse the request url which is relatie to the application root path.
+define("REQ_URL", substr(read_server_var("REDIRECT_URL"), strlen(APP_ROOT_PATH)));
+define("REQ_URL_DIR", dirname(REQ_URL));
+define("REQ_URL_BASE", basename(REQ_URL));
+define("REQ_URL_QUERY", REQ_URL . (isset($_SERVER["REDIRECT_QUERY_STRING"])? "?" . $_SERVER["REDIRECT_QUERY_STRING"]: ""));
