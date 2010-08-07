d63ac54e102695f19c3aee5a07fb3290035551f4
Hannes Landeholm <hannes@recurringly.com>
2010-08-07 11:50:11 +0000

    Added extra checks that protects against corrupt next_flash in session.

--- core/modules/messenger/module.php
+++ core/modules/messenger/module.php
@@ -2,8 +2,8 @@
 
 class MessengerModule extends \nmvc\CoreModule {
     public static function beforeRequestProcess() {
-        if (isset($_SESSION['next_flash'])) {
-            list($message, $status) = $_SESSION['next_flash'];
+        if (isset($_SESSION['next_flash']) && count($_SESSION['next_flash']) == 2) {
+            list($message, $status) = array_values($_SESSION['next_flash']);
             show_message($message, $status);
             unset($_SESSION['next_flash']);
         }
