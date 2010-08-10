98f5885e6a793cacfa64d9dbfa79b87707372798
Hannes Landeholm <hannes@recurringly.com>
2010-08-10 18:07:40 +0000

    Adding repair action.

--- core/modules/core/controllers/action_controller.php
+++ core/modules/core/controllers/action_controller.php
@@ -7,8 +7,15 @@ class ActionController extends DeveloperController {
     public function sync() {
         // Display all SQL queries made during syncronization.
         \nmvc\db\enable_display();
-        \nmvc\Model::syncronize_all_models();
+        \nmvc\Model::syncronizeAllModels();
         die("\n\n\n>>> Database syncronization complete!");
+    }
+
+    public function repair() {
+        // Display all SQL queries made during repair.
+        \nmvc\db\enable_display();
+        \nmvc\Model::repairAllModels();
+        die("\n\n\n>>> Model repairation complete!");
     }
 
     public function export() {
