704c078851cf6e89cbacc7a3e6038a38c267604a
Hannes Landeholm <hannes@recurringly.com>
2010-08-13 15:51:20 +0000

    Fixed bug where messenger would drop messages if request did redirect.

--- core/modules/messenger/module.php
+++ core/modules/messenger/module.php
@@ -3,9 +3,11 @@
 class MessengerModule extends \nmvc\CoreModule {
     public static function beforeRequestProcess() {
         if (isset($_SESSION['next_flash']) && count($_SESSION['next_flash']) == 2) {
-            list($message, $status) = array_values($_SESSION['next_flash']);
-            show_message($message, $status);
-            unset($_SESSION['next_flash']);
+            \nmvc\Controller::registerBeforeFirstRenderEvent(function() {
+                list($message, $status) = array_values($_SESSION['next_flash']);
+                show_message($message, $status);
+                unset($_SESSION['next_flash']);
+            });
         }
     }
 }
