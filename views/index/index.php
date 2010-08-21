16378badbec41122181602f3752205a5b3436e32
Hannes Landeholm <hannes@recurringly.com>
2010-08-21 15:47:19 +0000

    Improving the start page a little bit.

--- views/index/index.php
+++ views/index/index.php
@@ -1,6 +1,16 @@
 <?php namespace nmvc; ?>
+<?php $this->layout->enterSection("head"); ?>
+    <title>Welcome to nanoMVC!</title>
+<?php $this->layout->exitSection(); ?>
 <h1>Welcome to nanoMVC!</h1>
 <p>
-    This start page are planned to be replaced with an operational example
-    application.
+    This (not very impresive) start page are planned to be replaced with an
+    operational example application in a future version. If you want
+    to contribute with a better "start application", feel free to submit one!
+</p>
+<p>
+    However NanoMVC can still do cool stuff. Read 
+    <a href="http://docs.nanomvc.com/">
+        the documentation for more information!
+    </a>
 </p>
