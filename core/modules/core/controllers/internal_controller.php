6ff1de9756e38fd8b00615751563325c5b12c912
Hannes Landeholm <hannes@recurringly.com>
2010-08-23 12:47:17 +0000

    Fixed potential minor bug where application defined controller callback logic could break requests to the internal controllers.

--- core/modules/core/controllers/internal_controller.php
+++ core/modules/core/controllers/internal_controller.php
@@ -6,4 +6,11 @@
  */
 abstract class InternalController extends \nmvc\AppController {
 
+    // De-prototype any application defined callback logic that can
+    // break the request for any internal controller.
+    
+    public function beforeFilter($action_name, $arguments) {}
+    public function beforeRender($action_name, $arguments) {}
+    public function afterRender($action_name, $arguments) {}
+    
 }
