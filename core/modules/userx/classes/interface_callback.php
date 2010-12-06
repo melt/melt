69c8c07609d96ca9d741fc1dc3bfca3166ebdd82
Per Jonsson <per@recurringly.com>
2010-12-06 20:19:47 +0000

    Updated dashboard layout

--- core/modules/userx/classes/interface_callback.php
+++ core/modules/userx/classes/interface_callback.php
@@ -8,7 +8,7 @@ abstract class InterfaceCallback_app_overrideable extends \nmvc\qmi\InterfaceCal
         $instances = $this->getInstances();
         $user = $instances['nmvc\userx\UserModel'][0];
         if (login_challenge($user->username, $user->password, $user->remember_login)) {
-            \nmvc\messenger\redirect_message($this->getSuccessUrl(), __("You are now logged in."), "good");
+            \nmvc\messenger\redirect_message($this->getSuccessUrl(), __("Successfully logged in."), "good");
         } else {
             $user->password = "";
             $this->pushError($user, "password", __("Invalid username or password!"));
