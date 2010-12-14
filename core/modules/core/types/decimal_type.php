45333939cc2b218d392a58ebe337ab4591682ce0
Hannes Landeholm <hannes@recurringly.com>
2010-12-14 15:56:16 +0000

    Fixed bug where scale would be equal to precision.

--- core/modules/core/types/decimal_type.php
+++ core/modules/core/types/decimal_type.php
@@ -12,7 +12,7 @@ class DecimalType extends \nmvc\AppType {
     public function __construct($column_name, $precision = 20, $scale = 10) {
         parent::__construct($column_name);
         $this->precision = \intval($precision);
-        $this->scale = \intval($precision);
+        $this->scale = \intval($scale);
         if ($this->precision < 0)
             \trigger_error("The precision must be a positive integer.", \E_USER_ERROR);
         if ($this->scale < 0 || $this->scale > $this->precision)
