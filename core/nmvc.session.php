f28cd15c0d49afaa04f6217daee9f7ff57ccc11f
Hannes Landeholm <hannes@recurringly.com>
2011-01-03 13:24:14 +0000

    Fixed deadlock issue with sessions. Using row locking would be too expensive since session data is read for all requests, so data is instead read snapshotted. As previously, nanomvc simply does not guarantee sequential, non-overlapping, read-write access of session data. If data requires this property is should not be stored in $_SESSION.

--- core/nmvc.session.php
+++ core/nmvc.session.php
@@ -1,19 +1,24 @@
 <?php namespace nvmc\internal;
 
 call_user_func(function() {
-    // Saving sessions in database.
+    // Saving sessions in database. Session data is stored and read in
+    // snapshots. No row locking is used when reading since that would result
+    // in terrible performance as sessions are implicitly read
+    // and updated for all requests.
     \session_set_save_handler(function() {}, function() {
     },function($id) {
         if (REQ_IS_CORE_DEV_ACTION || defined("NMVC_REQUEST_COMPLETE"))
             return \serialize(array());
-        // Read.
-        $session_data = \nmvc\core\SessionDataModel::select()->where("session_key")->is($id)->first();
-        return $session_data !== null? $session_data->session_data: null;
+        // Read snapshot.
+        $session_data = \nmvc\Model::getDataForSelection(
+        \nmvc\core\SessionDataModel::select("session_data")
+        ->where("session_key")->is($id)->limit(1));
+        return isset($session_data[0][0])? $session_data[0][0]: null;
     }, function($session_key, $binary_session_data) {
         if (REQ_IS_CORE_DEV_ACTION || defined("NMVC_REQUEST_COMPLETE"))
             return;
         // Write.
-        $session_data = \nmvc\core\SessionDataModel::select()->where("session_key")->is($session_key)->first();
+        $session_data = \nmvc\core\SessionDataModel::select()->where("session_key")->is($session_key)->forUpdate()->first();
         if ($session_data === null) {
             $session_data = new \nmvc\core\SessionDataModel();
             $session_data->session_key = $session_key;
@@ -24,14 +29,14 @@ call_user_func(function() {
         if (REQ_IS_CORE_DEV_ACTION || defined("NMVC_REQUEST_COMPLETE"))
             return;
         // Destroy.
-        \nmvc\core\SessionDataModel::select()->where("session_key")->is($id)->unlink();
+        \nmvc\core\SessionDataModel::select()->where("session_key")->is($id)->forUpdate()->unlink();
     }, function($maxlifetime) {
         if (REQ_IS_CORE_DEV_ACTION || defined("NMVC_REQUEST_COMPLETE"))
             return;
         // GC.
         $maxlifetime = intval($maxlifetime);
         $time = time();
-        \nmvc\core\SessionDataModel::select()->where("last_store_attempt")->isLessThan($time - $maxlifetime)->unlink();
+        \nmvc\core\SessionDataModel::select()->where("last_store_attempt")->isLessThan($time - $maxlifetime)->forUpdate()->unlink();
     });
     // Set session ID by get parameter if set. This enables perserving
     // sessions when doing cross domain redirection.
