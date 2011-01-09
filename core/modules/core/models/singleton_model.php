d24cbfe7fd61c759e149c21ab1cd57ecf3f7eb02
Hannes Landeholm <hannes@recurringly.com>
2011-01-09 19:04:50 +0000

    Made core\SingletonModel forUpdate compatible.

--- core/modules/core/models/singleton_model.php
+++ core/modules/core/models/singleton_model.php
@@ -12,12 +12,15 @@ abstract class SingletonModel extends \nmvc\AppModel {
      * Returns this SingletonModel instance.
      * This function ensures that exactly one exists.
      */
-    public static function get() {
+    public static function get($for_update = false) {
         static $singleton_model_cache = array();
         $class_name = get_called_class();
         if (isset($singleton_model_cache[$class_name]))
             return $singleton_model_cache[$class_name];
-        $result = static::select()->all();
+        $selection = static::select();
+        if ($for_update)
+            $selection->forUpdate();
+        $result = $selection->all();
         $singleton_model = \reset($result);
         if ($singleton_model === false) {
             // If there are no model instance, create new.
