10a15284a00feb0261812ae8890e3bf9cf42349a
Per Jonsson <per@recurringly.com>
2011-01-10 10:31:25 +0000

    Added optional localization to maintenance message

--- core/modules/core/views/developer/_maintenance_info.php
+++ core/modules/core/views/developer/_maintenance_info.php
@@ -1,10 +1,10 @@
 <?php namespace nmvc\core; ?>
 <?php $this->display("/core/developer/error_layout"); ?>
 <h1>
-    Temporary Maintenance
+    <?php echo _("Temporary Maintenance"); ?>
 </h1>
 <p>
-    The site is currently offline due to temporary maintenance.
+    <?php echo _("The site is currently offline due to temporary maintenance."); ?>
 </p>
 <p>
     <?php echo $this->est; ?>
