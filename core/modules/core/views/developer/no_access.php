fe589e05a68c4c0b4fe54ac63ab2aeb21642bced
Per Jonsson <per@recurringly.com>
2011-01-20 12:14:33 +0000

    Added localization to access denied page

--- core/modules/core/views/developer/no_access.php
+++ core/modules/core/views/developer/no_access.php
@@ -1,14 +1,11 @@
 <?php namespace nmvc\core; ?>
 <?php $this->display("/core/developer/error_layout"); ?>
 <h1>
-    Access Denied - Function Disabled
+    <?php echo _("Access Denied - Function Disabled"); ?>
 </h1>
 <p>
-    This function can only be accessed by activating maintenance mode
-    and authorizing with the correct developer key.
+    <?php echo _("This function can only be accessed by activating maintenance mode and authorizing with the correct developer key."); ?>
 </p>
 <p>
-    Please refer to the
-    <a href="http://nanomvc.com/documentation">nanomvc documentation</a>
-    for more information.
+    <?php echo _("Please refer to the <a %s>NanoMVC Documentation</a> for more information.",'href="http://nanomvc.com/documentation"'); ?>
 </p>
