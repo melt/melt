9f5cb5bc110edfa3ff904499fdfb5e2648952188
Hannes Landeholm <hannes@recurringly.com>
2011-02-03 21:26:29 +0000

    SelectType now supports any scalar application keys which are hashed to to a 32 bit integers for storage if they are not booleans or integers.

--- core/modules/core/types/select_type.php
+++ core/modules/core/types/select_type.php
@@ -3,20 +3,41 @@
 /** SelectType, for selecting multiple options. */
 class SelectType extends \nmvc\AppType {
     private $options;
+    private $hash_to_key_map = array();
+    private $key_to_hash_map = array();
 
     public function __construct($column_name, $options = null) {
         parent::__construct($column_name);
         $this->options = $options;
         if (!\is_string($options))
-            $this->checkOptions();
+            $this->finalizeOptions();
     }
 
-    private function checkOptions() {
+    private function keyToHash($key) {
+        if (!\is_scalar($key))
+            return 0;
+        if (\is_bool($key)) {
+            $khash = $key? 1: 0;
+        } else if (\is_integer($key)) {
+            $khash = $key;
+        } else {
+            $khash = \unpack("l", \sha1($key, true));
+            $khash = \reset($khash);
+        }
+        return $khash;
+    }
+
+    private function finalizeOptions() {
         if (!is_array($this->options) || count($this->options) == 0)
-            trigger_error(__CLASS__ . " expects an array array of at least one option. Got: " . gettype($this->options), \E_USER_ERROR);
-        foreach ($this->options as $k => $v) {
-            if (!is_integer($k))
-                trigger_error("All keys in the " . __CLASS__ . " options array has to be integers. Found: " . gettype($k));
+            \trigger_error(__CLASS__ . " expects an array array of at least one option. Got: " . gettype($this->options), \E_USER_ERROR);
+        foreach ($this->options as $key => $value) {
+            if (!\is_scalar($key))
+                \trigger_error("All keys in the " . __CLASS__ . " options array has to be scalar. Found: " . gettype($key));
+            $khash = $this->keyToHash($key);
+            if (\array_key_exists($khash, $this->hash_to_key_map))
+                \trigger_error("Hash collision for two keys! You should avoid using strings as keys as their corresponding stored value is undefined. Use integers instead.", \E_USER_ERROR);
+            $this->hash_to_key_map[$khash] = $key;
+            $this->key_to_hash_map[$key] = $khash;
         }
     }
 
@@ -26,7 +47,7 @@ class SelectType extends \nmvc\AppType {
         if (!\is_callable(array($this->parent, $this->options)))
             \trigger_error(__CLASS__ . " configured incorrectly! Parent " . \get_class($this->parent) . " has no function " . $this->options, \E_USER_ERROR);
         $this->options = \call_user_func(array($this->parent, $this->options));
-        $this->checkOptions();
+        $this->finalizeOptions();
     }
 
     public function getInterface($name) {
@@ -37,9 +58,9 @@ class SelectType extends \nmvc\AppType {
         $selected = ' selected="selected"';
         foreach ($this->options as $option_key => $option_val) {
             $label = escape($option_val);
-            $id = escape($option_key);
-            $s = ($this->value == $id)? $selected: null;
-            $html .= "<option$s value=\"$id\">$label</option>";
+            $khash = escape($this->key_to_hash_map[$option_key]);
+            $s = ($this->value == $option_key)? $selected: null;
+            $html .= "<option$s value=\"$khash\">$label</option>";
         }
         $html .= "</select>";
         return $html;
@@ -48,27 +69,25 @@ class SelectType extends \nmvc\AppType {
     /** Make sure GET never returns an invalid value for this type. */
     public function get() {
         $this->prepareOptions();
-        if (isset($this->options[$this->value])) {
-            return intval($this->value);
+        if (isset($this->key_to_hash_map[$this->value])) {
+            return $this->value;
         } else {
-            reset($this->options);
-            return key($this->options);
+            return \reset($this->hash_to_key_map);
         }
     }
 
     public function set($value) {
         $this->prepareOptions();
-        $value = intval($value);
-        if (!isset($this->options[$value]))
-            trigger_error("Value '$value' is not a valid integer key for " . __CLASS__ . ".", \E_USER_ERROR);
+        if (!\is_scalar($value) || !isset($this->key_to_hash_map[$value]))
+            trigger_error("Value '$value' is not a valid key for " . __CLASS__ . ".", \E_USER_ERROR);
         $this->value = $value;
     }
 
     public function readInterface($name) {
         $this->prepareOptions();
-        $this->value = @$_POST[$name];
-        if (!isset($this->options[$this->value]))
-            $this->value = null;
+        $khash = @$_POST[$name];
+        if (isset($this->hash_to_key_map[$khash]))
+            $this->value = $this->hash_to_key_map[$khash];
     }
 
     public function getLabel() {
@@ -86,10 +105,13 @@ class SelectType extends \nmvc\AppType {
     }
 
     public function getSQLValue() {
-        return intval($this->value);
+        return $this->key_to_hash_map[$this->value];
     }
 
     public function setSQLValue($value) {
-        $this->value = intval($value);
+        if (isset($this->hash_to_key_map[$value]))
+            $this->value = $this->hash_to_key_map[$value];
+        else
+            $this->value = \reset($this->hash_to_key_map);
     }
 }
