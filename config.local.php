022c05c4627ba004d95878bbe5e212718f2de177
Hannes Landeholm <hannes@recurringly.com>
2011-02-04 22:15:47 +0000

    Renaming bootstrapping files so they can be used out of the box.

---
+++ config.local.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * This file is loaded before config.php if it exists.
+ *
+ * Put configuration directives here that override those in config.php
+ *
+ * This is useful for having a local development configuration that overrides
+ * the production configuration.
+ */
---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Contains the configuration for this application.
+ *
+ * nanoMVC configuration options are named constants that resides in the
+ * namespace \nanomvc\$MODULE\config where $MODULE is the module that uses the
+ * configuration option.
+ *
+ * NanoMVC will automatically insert missing directives into this file
+ * so don't be suprised when it starts editing itself.
+ */
+
+namespace nmvc\internal {
+    modules_using(
+        // Add modules that your application uses here.
+    );
+}
