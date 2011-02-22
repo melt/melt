960f737f202cada9d78dae105563ed85c9575d84
Hannes Landeholm <hannes@recurringly.com>
2011-02-22 16:48:36 +0000

    TimestampType now takes DateTime object. Syntax improvments. Replaced deprecated lambda-style function creation with closure.

--- core/modules/core/types/timestamp_type.php
+++ core/modules/core/types/timestamp_type.php
@@ -4,12 +4,18 @@ class TimestampType extends \nmvc\AppType {
     public $only_date = "false";
     public $title = "";
 
+    public function set($value) {
+        parent::set(($value instanceof \DateTime)? $value->getTimestamp(): \intval($value));
+    }
+
     public function getSQLType() {
         return "bigint";
     }
+
     public function getSQLValue() {
-        return intval($this->value);
+        return \intval($this->value);
     }
+
     public function getInterface($name) {
         $title = ($this->title != "")? ' title="' . $this->title . '" ': '';
         $dateonly = $this->only_date == "true";
@@ -22,33 +28,34 @@ class TimestampType extends \nmvc\AppType {
             . "<br /><span style=\"font-size: 9px;\">" . __('Timestamp Format')
             . ": " . $date_syntax_helper . ($dateonly? "": $time_syntax_helper) . "</span>";
     }
+
     public function readInterface($name) {
-        $newstamp = strval(@$_POST[$name]);
+        $newstamp = \strval(@$_POST[$name]);
         // Get the numeric clusters.
-        $m = preg_split('#[^0-9]+#', $newstamp);
+        $m = \preg_split('#[^0-9]+#', $newstamp);
         // Filter all empty positions.
-        $m = array_values(array_filter($m, create_function('$val', 'return $val !== \'\';')));
+        $m = \array_values(\array_filter($m, function($val) { return $val !== ''; }));
         // Make timestamp.
         $dateonly = $this->only_date == "true";
         if ($dateonly) {
-            if (count($m) == 3) {
-                $yr = intval($m[0]);
-                $mo = intval($m[1]);
-                $d = intval($m[2]);
-                $time = mktime(0, 0, 0, $mo, $d, $yr);
+            if (\count($m) == 3) {
+                $yr = \intval($m[0]);
+                $mo = \intval($m[1]);
+                $d = \intval($m[2]);
+                $time = \mktime(0, 0, 0, $mo, $d, $yr);
                 if ($time === false || $time === -1)
                     $time = 0;
             } else
                 $time = 0;
         } else {
-            if (count($m) == 6) {
-                $yr = intval($m[0]);
-                $mo = intval($m[1]);
-                $d = intval($m[2]);
-                $hr = intval($m[3]);
-                $mi = intval($m[4]);
-                $s = intval($m[5]);
-                $time = mktime($hr, $mi, $s, $mo, $d, $yr);
+            if (\count($m) == 6) {
+                $yr = \intval($m[0]);
+                $mo = \intval($m[1]);
+                $d = \intval($m[2]);
+                $hr = \intval($m[3]);
+                $mi = \intval($m[4]);
+                $s = \intval($m[5]);
+                $time = \mktime($hr, $mi, $s, $mo, $d, $yr);
                 if ($time === false || $time === -1)
                     $time = 0;
             } else
@@ -56,12 +63,13 @@ class TimestampType extends \nmvc\AppType {
         }
         $this->value = intval($time);
     }
+
     public function __toString() {
-        $value = intval($this->value);
+        $value = \intval($this->value);
         if ($value == 0)
             return "";
         $dateonly = $this->only_date == "true";
-        return date(!$dateonly? 'Y-m-d, H:i:s e': 'Y-m-d', $value);
+        return \date(!$dateonly? 'Y-m-d, H:i:s e': 'Y-m-d', $value);
     }
 
 }
