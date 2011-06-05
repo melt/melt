7d30d4bf1fa9e7501a77d11e3c1b6730a7c71de1
Hannes Landeholm <hannes@recurringly.com>
2011-06-05 08:26:11 +0000

    Core messenger module now uses dialogs by default.

--- core/modules/messenger/views/message.php
+++ core/modules/messenger/views/message.php
@@ -1,25 +1,34 @@
 <?php namespace nmvc\messenger; ?>
 <?php $this->layout->enterSection("head"); ?>
-<script type="text/javascript" src="<?php echo url("/static/cmod/messenger/blend.js"); ?>"></script>
-<link rel="stylesheet" type="text/css" href="<?php echo url("/static/cmod/messenger/msg_bar.css"); ?>" />
-<script type="text/javascript">
-    setTimeout('showBar()', 4500);
-    setTimeout('hideBar()', 5000);
-    function showBar() {
-        // Blend out the flasher from the screen.
-        opacity('message_bar', 100, 0, 450);
-    }
-    function hideBar() {
-        // Make sure it's hidden. Even on older browsers.
-        var e = document.getElementById('message_bar');
-        e.style.visibility = 'hidden';
-        e.style.width = 0;
-        e.style.height = 0;
-    }
-</script>
+    <script type="text/javascript">
+        $(function() {
+            window.setTimeout(function() {
+                $("#request-message-dialog").dialog({
+                    autoOpen: true,
+                    closeOnEscape: true,
+                    resizable: false,
+                    modal: true,
+                    width: 400,
+                    buttons: {
+                        <?php echo json_encode(_("Ok")); ?>: function() {
+                            $(this).dialog("destroy");
+                        }
+                    }
+                });
+                $(".ui-widget-overlay").click(function() {
+                    $("#request-message-dialog").dialog("destroy");
+                });
+            }, 50);
+        });
+    </script>
 <?php $this->layout->exitSection(); ?>
 <?php $this->layout->enterSection("content_head"); ?>
-<div onclick="this.style.visibility = 'hidden';" class="message_bar msg_bar_status_<?php echo $this->status; ?>" id="message_bar">
-    <?php echo $this->message; ?>
-</div>
+    <div id="request-message-dialog" title="<?php echo ($this->status == "bad")? _("Error"): _("Message");?>" style="display: none;">
+        <div class="dialog_content">
+            <div class="message">
+                <img alt="" src="<?php echo url("/static/cmod/messenger/" . (($this->status == "bad")? "no.png": "ok.png")); ?>" style="float: left; margin: 0px 8px 8px 0px;" />
+                <?php echo $this->message; ?>
+            </div>
+        </div>
+    </div>
 <?php $this->layout->exitSection(); ?>
