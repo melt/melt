b5ee43b48ca66318cedc4d117c46ab6b512f8229
Hannes Landeholm <hannes@recurringly.com>
2011-08-01 01:25:47 +0200

    Bootstrapper now generates the configuration absolutly nessecary by itself and does not initialize any application data.

--- index.php
+++ index.php
@@ -27,39 +27,90 @@ $app_is_64bit = PHP_INT_MAX > 0x7FFFFFFF;
 if (!$app_is_64bit && !isset($_GET["ignore_32_warning"]))
     fail("You are not using 64 bit PHP! 32 bit PHP does not support 64 bit integers which has several problems. The most serious problem is that the application will eventually crash when the ID address space runs out. 64 bit PHP also has better performance and memory support. There is no reason to run 32 bit PHP on a 64 bit machine. If you undestand the risks and want to use 32 bit PHP anyway, <a href=\"?ignore_32_warning=1\">click here to continue</a>. <b>(Not recommended for production setups!)</b><br />");
 chdir(__DIR__);
-if (!is_dir("core/scaffolding"))
-    fail("Could not find folder 'core/scaffolding'!");
-foreach (glob("core/scaffolding/*", GLOB_BRACE) as $file) {
-    if (file_exists(basename($file))) {
-        print "'$file' already exists, ignoring...<br />";
-        continue;
-    }
-    if (!is_dir($file)) {
-        print "Creating file \"$file\".<br />";
-        copy($file, basename($file))
-        or fail("Could not create file \"" . basename($file) . "\". Incorrect file system permissions?");
-    }
-}
-$htaccess_content = file_get_contents("core/scaffolding/.htaccess");
 $rewrite_base = str_replace("\\", "/", dirname(@$_SERVER["PHP_SELF"]));
 print "The application seem to be located in path '$rewrite_base'. Writing this to .htaccess RewriteBase directive.<br />";
-$htaccess_content = str_replace("RewriteBase /", "RewriteBase $rewrite_base", $htaccess_content);
+$htaccess_content = <<<EOD
+# Only utf-8 policy.
+AddDefaultCharset utf-8
+
+# Turn off indexing and directory slash correction.
+Options -Indexes
+DirectorySlash Off
+
+# Enable symlinks.
+Options +FollowSymLinks
+
+# Start the rewrite engine.
+RewriteEngine On
+
+# Use melt to display error documents.
+ErrorDocument 400 /core/core.php
+ErrorDocument 401 /core/core.php
+ErrorDocument 402 /core/core.php
+ErrorDocument 403 /core/core.php
+ErrorDocument 404 /core/core.php
+ErrorDocument 405 /core/core.php
+ErrorDocument 406 /core/core.php
+ErrorDocument 407 /core/core.php
+ErrorDocument 408 /core/core.php
+ErrorDocument 409 /core/core.php
+ErrorDocument 410 /core/core.php
+ErrorDocument 411 /core/core.php
+ErrorDocument 412 /core/core.php
+ErrorDocument 413 /core/core.php
+ErrorDocument 414 /core/core.php
+ErrorDocument 415 /core/core.php
+ErrorDocument 416 /core/core.php
+ErrorDocument 417 /core/core.php
+ErrorDocument 500 /core/core.php
+ErrorDocument 501 /core/core.php
+ErrorDocument 502 /core/core.php
+ErrorDocument 503 /core/core.php
+ErrorDocument 504 /core/core.php
+ErrorDocument 505 /core/core.php
+
+# Set this to the directory this application is installed in.
+RewriteBase $rewrite_base
+
+# Rewrite static module files to their appropriate locations.
+RewriteRule ^static/cmod/([^/]+)/(.*)$ core/modules/$1/static/$2 [L]
+RewriteRule ^static/mod/([^/]+)/(.*)$ modules/$1/static/$2 [L]
+
+# Rewrite everything non-static to melt core.
+RewriteCond %{REQUEST_URI} !/static
+RewriteRule .* core/core.php [L]
+EOD;
 print "Writing .htaccess file...<br />";
 file_put_contents(".htaccess", $htaccess_content);
 $dev_key = "";
 for ($i = 0; $i < 16; $i++)
     $dev_key .= mt_rand() . "-";
 $dev_key = substr(sha1($dev_key), 0, 12);
-$config = file_get_contents("config.php");
-$config .= "\n\n" . 'namespace melt\core\config {' . "\n    " . 'const DEVELOPER_KEY = ' . var_export($dev_key, true) . ';';
-if (!$app_is_64bit)
-    $config .= "\n    " . 'const IGNORE_64_BIT_WARNING = true;';
-$config .= "\n}\n";
+$dev_key_config = var_export($dev_key, true);
+$additional_config = $app_is_64bit? "": "const IGNORE_64_BIT_WARNING = true;";
+$config = <<<EOD
+<?php
+/**
+ * This file is should contain your local, environment specific configuration.
+ * Put all all of those here here (like MySQL configuration)
+ * and make sure to ignore this file in your versioning system if you are
+ * using one.
+ * 
+ * Any configuration directives here override those in config.php
+ * because it is loaded before it.
+ */
+
+namespace melt\core\config {
+    const MAINTENANCE_MODE = true;
+    const DEVELOPER_KEY = $dev_key_config;
+    $additional_config
+}
+EOD;
 print "Writing initial configuration...<br />";
-file_put_contents("config.php", $config);
+file_put_contents("config.local.php", $config);
 print "Deleting bootstrap script.<br />";
-unlink(__FILE__)
-or fail("Could not delete \"" . __FILE__ . "\". Incorrect file system permissions?");
+//unlink(__FILE__)
+//or fail("Could not delete \"" . __FILE__ . "\". Incorrect file system permissions?");
 print "</pre>";
 ?>
 <h2 class="subtitle">Bootstrapping <span>completed</span></h2>
