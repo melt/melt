2b907706e91e56202383fea327836fdc821cfafd
Hannes Landeholm <hannes@recurringly.com>
2011-08-03 18:55:28 +0200

    Fixed bug where console deploy commands would fail out on platforms with unreasonable default http timeout settings.

--- core/core/controllers/console_controller.php
+++ core/core/controllers/console_controller.php
@@ -7,6 +7,10 @@ class ConsoleController extends InternalController {
         parent::beforeRender($action_name, $arguments);
         if ($action_name !== "index" && !APP_IN_DEVELOPER_MODE)
             \melt\request\show_xyz(403);
+        // Make sure http timeout is reasonable.
+        stream_context_set_default(array(
+            "http" => array("timeout" => 20)
+        ));
     }
 
     public function index() {
@@ -326,49 +330,42 @@ class ConsoleController extends InternalController {
     
     private function ghDeploy($user, $repo, &$target_tag, $repo_description_tag, $prepare_fn) {
         // Get repository info.
-        $repo_info = file_get_contents("https://api.github.com/repos/$user/$repo");
+        $repo_info = @file_get_contents("https://api.github.com/repos/$user/$repo");
         if ($repo_info === false)
             die("Error: Specified repository not found (or not tagged).\n");
         $repo_info = json_decode($repo_info);
         if (strpos($repo_info->description, $repo_description_tag) === false)
             die("Error: Did not find $repo_description_tag in repo description. The repository is not a valid target.\n");
-        if (strcasecmp($target_tag, "head") === 0) {
-            // Using head.
-            $tarball_url = "https://github.com/$user/$repo/tarball/master";            
-        } else {
-            // Using specific tag.
-            $tags_info = @file_get_contents("https://api.github.com/repos/$user/$repo/tags");
-            if ($tags_info === false)
-                die("Error: Specified repository not found (or not tagged).\n");
-            $tags_info = json_decode($tags_info);
-            if (count($tags_info) === 0)
-                die("Error: Specified repository does not have any tags.\n");
-            $tags_index = array();
-            foreach ($tags_info as $tag_info)
-                $tags_index[$tag_info->name] = $tag_info;
-            if ($target_tag !== null && !isset($tags_index[$target_tag]))
-                die("Error: Specified tag/version does not exist in repository.\n");
-            uksort($tags_index, function($v1, $v2) {
-                return strnatcasecmp($v2, $v1);
-            });
-            if (\melt\string\ends_with((string) $target_tag, "*")) {
-                echo "All tags in repository:\n";
-                foreach ($tags_index as $tag => $tag_info)
-                    echo "$tag ";
-                die("\n");
-            }
-            $prepare_fn();
-            reset($tags_index);
-            $target_tag = $target_tag !== null? $target_tag: key($tags_index);
-            $tag_info = $tags_index[$target_tag];
-            $tarball_url = $tag_info->tarball_url;
+        $tags_info = @file_get_contents("https://api.github.com/repos/$user/$repo/tags");
+        if ($tags_info === false)
+            die("Error: Specified repository not found (or not tagged).\n");
+        $tags_info = json_decode($tags_info);
+        if (count($tags_info) === 0)
+            die("Error: Specified repository does not have any tags.\n");
+        $tags_index = array();
+        foreach ($tags_info as $tag_info)
+            $tags_index[$tag_info->name] = $tag_info;
+        if ($target_tag !== null && !isset($target_tag[$target_tag]))
+            die("Error: Specified tag/version does not exist in repository.\n");
+        uksort($tags_index, function($v1, $v2) {
+            return strnatcasecmp($v2, $v1);
+        });
+        if (\melt\string\ends_with((string) $target_tag, "*")) {
+            echo "All tags in repository:\n";
+            foreach ($tags_index as $tag => $tag_info)
+                echo "$tag ";
+            die("\n");
         }
+        $prepare_fn();
+        reset($tags_index);
+        $target_tag = $target_tag !== null? $target_tag: key($tags_index);
+        $tag_info = $tags_index[$target_tag];
         $local_path = APP_DIR . "/ghd-deploy-tmp.tar.gz";
         if (is_file($local_path)) {
             if (!@unlink($local_path))
                 die("Could not delete $local_path!\n");
         }
-        $this->downloadFile($tarball_url, $local_path);
+        $this->downloadFile($tag_info->tarball_url, $local_path);
         return $local_path;
     }
     
@@ -419,7 +416,6 @@ class ConsoleController extends InternalController {
         if (rename(APP_DIR . "/$internal_path/core", APP_DIR . "/core") === false)
             die("Could not rename /$internal_path/core module folder to /core.");
         unlink_recursive(APP_DIR . "/$internal_path", function() {});
-        @unlink(APP_DIR . "/pax_global_header");
         die("Melt core $target_tag was successfully deployed. Please reload the console now by typing \"reload\".\n");        
     }
     
