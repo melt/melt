bdec984358748fd14ac6498d2b9b5dd91c8aa4a4
Hannes Landeholm <hannes@recurringly.com>
2011-08-11 22:45:27 +0200

    Modified DateType to use melt\core\DateType instead of PHP DateType. Fixed lots of subtle bugs by reimplementing it. For example, the new implementation does not crash when reading bad values from interface.

--- core/core/types/date_type.php
+++ core/core/types/date_type.php
@@ -5,11 +5,18 @@
  * It stores/gets/sets dates in ISO-8601 format (YYYY-mm-dd).
  */
 class DateType extends \melt\AppType {
-    private $varchar_size = null;
-
+    public $parsing_failed = false;
+    
     public function __construct() {
         parent::__construct();
-        $this->value = new \DateTime();
+        $this->resetDateTime();
+    }
+    
+    protected function resetDateTime() {
+        $this->value = DateTime::createFromFormat("U", 0);
+    }
+    
+    protected function resetTime() {
         $this->value->setTime(0, 0, 0);
     }
 
@@ -18,38 +25,38 @@ class DateType extends \melt\AppType {
     }
 
     public function get() {
-        return $this->value;
+        return new DateTime($this->value);
     }
 
     public function set($value) {
         if (\is_string($value) || \is_integer($value)) {
             if (\is_string($value)) {
                 $value = \preg_replace('#[^\d]#', '', $value);
-                if (\strlen($value) != 8)
-                    \trigger_error(__CLASS__ . " did not understand the given Date!", \E_USER_ERROR);
-                $time = @\mktime(null, null, null, \substr($value, 4, 2), \substr($value, 6, 2), \substr($value, 0, 4));
-                if ($time === false)
-                    \trigger_error(__CLASS__ . " did not understand the given Date!", \E_USER_ERROR);
+                if (\strlen($value) === 8) {
+                    $this->value->setDate(\substr($value, 0, 4), \substr($value, 4, 2), \substr($value, 6, 2));
+                } else {
+                    $this->parsing_failed = true;
+                }
             } else {
-                $time = $value;
+                $this->value = @DateTime::createFromFormat("U", $value);
+                if ($this->value === false) {
+                    $this->resetDateTime();
+                    $this->parsing_failed = true;
+                } else {
+                    $this->resetTime();
+                }
             }
-            $this->value = @\DateTime::createFromFormat("U", $time);
-            $this->value->setTime(0, 0, 0);
-            if ($this->value === false)
-                \trigger_error(__CLASS__ . " did not understand the given Date!", \E_USER_ERROR);
         } else if ($value instanceof \DateTime) {
-            $this->value = $value;
-            $this->value->setTime(0, 0, 0);
+            $this->value = new DateTime($value);
+            $this->resetTime();
         } else {
             \trigger_error(__CLASS__ . " did not understand the given Date!", \E_USER_ERROR);
         }
     }
-
+    
     public function setSQLValue($value) {
-        $this->value = @\DateTime::createFromFormat('Y-m-d', $value);
-        $this->value->setTime(0, 0, 0);
-        if ($this->value === false)
-            $this->value = new \DateTime();
+        $this->resetTime();
+        $this->set($value);
     }
 
     public function getSQLValue() {
@@ -58,10 +65,7 @@ class DateType extends \melt\AppType {
 
     public function getInterface($name) {
         $value = escape($this->value->format("Y-m-d"));
-        $maxlength = null;
-        if ($this->varchar_size !== null)
-            $maxlength = "maxlength=\"" . $this->varchar_size . "\"";
-        return "<input type=\"text\" $maxlength name=\"$name\" id=\"$name\" value=\"$value\" />"
+        return "<input type=\"text\" name=\"$name\" id=\"$name\" value=\"$value\" />"
         . "<script type=\"text/javascript\">$(function() { $('#$name').datepicker({ dateFormat: 'yy-mm-dd' }); });</script>";
     }
 
