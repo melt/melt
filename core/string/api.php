bfc3c08d0f82df7d196412a4b216dad08c3e28ee
Hannes Landeholm <hannes@recurringly.com>
2012-02-03 17:00:44 +0100

    Replaced email validator with a new RFC 822 compatible variant/implementation that should work better. The other validator was broken because it would not accept email addresses with dash in them.

--- core/string/api.php
+++ core/string/api.php
@@ -153,11 +153,26 @@ function hex_decode($str) {
 }
 
 /**
-* @desc Returns true if given email address is RFC 2822 compatible.
-*/
+ * @desc Returns true if given email address is RFC 822 compatible.
+ * @see http://www.iamcal.com/publish/articles/php/parsing_email/
+ * @copyright Tim Fletcher https://creativecommons.org/licenses/by-sa/2.5/
+ * (3rd party author which is neither related too, nor endorse this software)
+ */
 function email_validate($email) {
-    $emailregex = ";^(([a-zA-Z\d!#$%&'*+\-/=?^_`{|}~]+\x20*|\"((?=[\x01-\x7f])[^\"\\]|\\[\x01-\x7f])*\"\x20*)*(?<angle><))?((?!\.)(\.?[a-zA-Z\d!#$%&'*+\-/=?^_`{|}~]+)+|\"((?=[\x01-\x7f])[^\"\\]|\\[\x01-\x7f])*\")@(((?!-)[a-zA-Z\d\-]+(?<!-)\.)+[a-zA-Z]{2,}|\[(((?(?<!\[)\.)(25[0-5]|2[0-4]\d|[01]?\d?\d)){4}|[a-zA-Z\d\-]*[a-zA-Z\d]:((?=[\x01-\x7f])[^\\\[\]]|\\[\x01-\x7f])+)\])(?(angle)>)$;";
-    return preg_match($emailregex, $email) == 1;
+    $qtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]';
+    $dtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]';
+    $atom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c' .
+    '\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+';
+    $quoted_pair = '\\x5c[\\x00-\\x7f]';
+    $domain_literal = "\\x5b($dtext|$quoted_pair)*\\x5d";
+    $quoted_string = "\\x22($qtext|$quoted_pair)*\\x22";
+    $domain_ref = $atom;
+    $sub_domain = "($domain_ref|$domain_literal)";
+    $word = "($atom|$quoted_string)";
+    $domain = "$sub_domain(\\x2e$sub_domain)*";
+    $local_part = "$word(\\x2e$word)*";
+    $addr_spec = "$local_part\\x40$domain";
+    return preg_match("!^$addr_spec$!", $email)? 1: 0;
 }
 
 /**
