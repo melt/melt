2f7ed26a7c2f25cc4b6caf81e49dd1efc60f6b11
Per Jonsson <per.d.jonsson@gmail.com>
2012-03-15 16:50:24 +0100

    Updated core⁄\messenger by replacing dependencies to jQuery UI with dependencies to newly added Twitter Bootstrap.
    Fixed include of bootstrap.css to be included also when responsive CSS is true.

--- core/js/views/include_bootstrap.php
+++ core/js/views/include_bootstrap.php
@@ -1,9 +1,8 @@
 <?php namespace melt\js; ?>
 <?php $this->layout->enterSection("head_head"); ?>
+    <link type="text/css" href="<?php echo APP_IN_DEVELOPER_MODE? url("/static/cmod/js/twitter-bootstrap/css/bootstrap.css"): url("/static/cmod/js/twitter-bootstrap/css/bootstrap.min.css"); ?>" rel="stylesheet" />
     <?php if(config\TWITTER_BOOTSTRAP_RESPONSIVE_CSS): ?>
     <link type="text/css" href="<?php echo APP_IN_DEVELOPER_MODE? url("/static/cmod/js/twitter-bootstrap/css/bootstrap-responsive.css"): url("/static/cmod/js/twitter-bootstrap/css/bootstrap-responsive.min.css"); ?>" rel="stylesheet" />
-    <?php else: ?>
-    <link type="text/css" href="<?php echo APP_IN_DEVELOPER_MODE? url("/static/cmod/js/twitter-bootstrap/css/bootstrap.css"): url("/static/cmod/js/twitter-bootstrap/css/bootstrap.min.css"); ?>" rel="stylesheet" />
     <?php endif; ?>
     <script type="text/javascript" src="<?php echo APP_IN_DEVELOPER_MODE? url("/static/cmod/js/twitter-bootstrap/js/bootstrap.js"): url("/static/cmod/js/twitter-bootstrap/js/bootstrap.min.js"); ?>"></script>
 <?php $this->layout->exitSection(); ?>
